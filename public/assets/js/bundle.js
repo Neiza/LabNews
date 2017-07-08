2baac396eeeee194efba72b41a03c118ac34b6cb
Neiza Nunez <neizapf@gmail.com>
2017-07-07 21:49:39 -0500

    sass

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -3,15 +3,13 @@
 const Header = () => {
   const header = $('<header class="header"></header>');
   const head = $('<div class="header__elem header__elem--head"></div>');
-  const headSections = $('  <div class="header__sections"><img src="assets/img/menu.png" alt="" class="header__icon--menu">SECTIONS</div>');
+  const headSections = $('<div class="header__sections"><img src="assets/img/menu.png" alt="" class="header__icon--menu">  SECTIONS</div>');
   const headSearch = $('<div class="header__search"><img src="" alt="" class="header__icon header__icon--search">SEARCH</div>');
   const headRedes =$('<div class="header__redes"><img src="assets/img/fb.png" class="header__icon header__icon--face"><img class="header__icon header__icon--twitter"> <img src="assets/img/in.png" alt="" class="header__icon header__icon--in"></div>')
-
   const logoContainer = $('<div class="header__elem header__elem--logo "></div>');
   const imgLogo = $('<img src="assets/img/logoicon.png" alt="" class="header__logo">');
   const date = $('<span class="header__date">Lunes Junio, 12 de 20017</span>');
-  const weather = $('<img src="assets/img/cloud.png" alt="" class="header__logo"><span class="header__weather">22°</span>');
-
+  const weather = $('<img src="assets/img/cloud.png" alt="" class="header__icon header__icon--cloud"><span class="header__weather">22°</span>');
   const nav = $('<div class="header__elem header__elem--nav "></div>');
   const loUltimo = $('<a href="" class="header__navElem">Lo último</a>');
   const opinion = $('<a href="" class="header__navElem">Opinión</a>');
@@ -97,7 +95,7 @@ const NewsDetail = () => {
   const brief = $('<span class="detail__brief">'+ state.news[0].brief +'</span>')
   const img = $('<img class="detail__img" src="assets/img/news/' + state.news[0].img + ' "alt="">');
   const author = $('<div class="detail__author"></div>');
-  const imgAuthor = $('<img class="detail__author--img" src="" alt="">');
+  const imgAuthor = $('<img class="detail__author--img" src="assets/img/authors/'+state.news[0].author.picture+'" alt="">');
   const name = $('<span class="detail__name">'+ state.news[0].author.name+'</span>');
   const date = $('<span class="detail__date">22.06.17 12:26 pm</span>');
   const email = $('<span class="detail__email">@photosynthetica</span>');
--- src/assets/js/components/c1header.js
+++ src/assets/js/components/c1header.js
@@ -2,15 +2,13 @@
 const Header = () => {
   const header = $('<header class="header"></header>');
   const head = $('<div class="header__elem header__elem--head"></div>');
-  const headSections = $('  <div class="header__sections"><img src="assets/img/menu.png" alt="" class="header__icon--menu">SECTIONS</div>');
+  const headSections = $('<div class="header__sections"><img src="assets/img/menu.png" alt="" class="header__icon--menu">  SECTIONS</div>');
   const headSearch = $('<div class="header__search"><img src="" alt="" class="header__icon header__icon--search">SEARCH</div>');
   const headRedes =$('<div class="header__redes"><img src="assets/img/fb.png" class="header__icon header__icon--face"><img class="header__icon header__icon--twitter"> <img src="assets/img/in.png" alt="" class="header__icon header__icon--in"></div>')
-
   const logoContainer = $('<div class="header__elem header__elem--logo "></div>');
   const imgLogo = $('<img src="assets/img/logoicon.png" alt="" class="header__logo">');
   const date = $('<span class="header__date">Lunes Junio, 12 de 20017</span>');
-  const weather = $('<img src="assets/img/cloud.png" alt="" class="header__logo"><span class="header__weather">22°</span>');
-
+  const weather = $('<img src="assets/img/cloud.png" alt="" class="header__icon header__icon--cloud"><span class="header__weather">22°</span>');
   const nav = $('<div class="header__elem header__elem--nav "></div>');
   const loUltimo = $('<a href="" class="header__navElem">Lo último</a>');
   const opinion = $('<a href="" class="header__navElem">Opinión</a>');
